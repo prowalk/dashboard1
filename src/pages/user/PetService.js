357ea174d9986cb71e3e3150a23870a603d0fd5e
luis gonzalez <luisgonzalez18997@gmail.com>
2020-05-19 11:22:55 -0400

    add table in pet services

--- src/pages/user/PetService.js
+++ src/pages/user/PetService.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import NavBar from "../../components/navbar"
 import Footer from '../../components/footer'
+import InputSearch from '../../components/inputSeach'
+import Table from '../../components/table'
 
 const PetServices = () => {
+    const [search, setSearch] = useState('')
+    const titles = ["Service type","Service Date", "Affiliate", "Address", "Status", "Report", "$ Inovice", "Refund"]
+    const datas = [
+        {service_type: '', service_date: '',affiliate: '', addres:'3 Street', status: '',report: '',invoice:  '', refund: ''}
+    ]
+
+    const handleChange = ({target: {value}}) => {
+        setSearch(
+            value
+        )
+    }
+
 
     return(
         <>
             <NavBar/>
+            <div className='main container'>
+                <InputSearch value={search} handleChange={handleChange} />
+                <Table title={titles} values={datas} />
+            </div>
             <Footer/>
         </>
     )
